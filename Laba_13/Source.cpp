a362740c45092ad21f4a73ec7454a43c57bc5ce5
Azelisk <115938950+Azelisk@users.noreply.github.com>
2023-05-07 17:57:50 +0500

    Add files via upload

---
+++ Laba_13/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "Pair.h"
+#include "Ex1.h"
+#include "Ex2.h"
+#include "Ex3.h"
+
+int main()
+{
+	foo1();
+
+	foo2();
+
+	foo3();
+
+	return 0;
+}
